be81a965b081688863f18cd228f152d455eed640
Yatin Luthra <luthrayatin@gmail.com>
2017-03-24 09:17:18 +1000

    Adding the initial web application for Algorithms

---
+++ Algorithms/AlgorithmsWebApp/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(AlgorithmsWebApp.Startup))]
+namespace AlgorithmsWebApp
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
